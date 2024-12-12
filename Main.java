366bec32725d322ed97f1fb45f766685e86d8b07
Truejesuschrist <theshadowarchon@gmail.com>
2024-12-12 23:54:44 +0000

    dada

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ class Main
     System.out.println("Enter the amount of years until withdrawl:" );
     years = keyboard.nextInt();
     keyboard.nextLine();
-
+    keyboard.close();
     /*Output of interestCalc methods*/
     System.out.println("\nIterative Output");
     System.out.println("-----------------");
@@ -28,9 +28,9 @@ class Main
 
     //Uncomment this section to see your recursive method in action!
     
-    // System.out.println("\n\nRecursive Output");
-    // System.out.println("-----------------");
-    // System.out.printf("Your investment will be $%.2f after %d year(s) at a rate of %.1f%%", recursiveInterestCalc(amount, rate, years), years, rate*100);
+     System.out.println("\n\nRecursive Output");
+     System.out.println("-----------------");
+     System.out.printf("Your investment will be $%.2f after %d year(s) at a rate of %.1f%%", recursiveInterestCalc(amount, rate, years), years, rate*100);
   }
 
   /*iterativeInterestCalc method uses a for loop to calulate the new balance in your investment account.*/
@@ -47,7 +47,14 @@ class Main
 
 
   //Start coding here!!
-  /*recursiveInterestCalc method uses recursion to calulate the new balance in your investment account*/
-  
-
+  /*recursiveInterestCalc method uses recursion to calculate the new balance in your investment account*/
+  public static double recursiveInterestCalc(double amount, double rate, int years)
+  {
+    if (years == 0) {
+      return amount;
+    } else {
+      double newBalance = amount + amount*rate;
+      return recursiveInterestCalc(newBalance, rate, years-1);
+    }
+  }
 }
